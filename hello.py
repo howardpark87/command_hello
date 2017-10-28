8a879311417dd00b6b72200becdfc7b3af507b49
howardpark87 <33187864+howardpark87@users.noreply.github.com>
2017-10-29 08:55:18 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 // For command line git tutorial
-// Lopcal repository modifictaion
+// Remote repository modifictaion
 print ("hello world")
 print ("Master second Branch")
 print ("this is hotfix")
