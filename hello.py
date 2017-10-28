fcd7f78a15752526c5a84d2c4ed0262f9183bb76
Stark-PC\Stark <howardpark87@gmail.com>
2017-10-29 08:59:00 +0900

    tets

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 // For command line git tutorial
-// Remote repository modifictaion
+// Local repository modifictaion
 print ("hello world")
 print ("Master second Branch")
 print ("this is hotfix")
